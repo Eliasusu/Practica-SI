ef9c174108dae15f20fd73aaec45ee1bfddb0d21
Elias <eliasrenepalamidessi@gmail.com>
2024-07-18 14:56:43 -0300

    Completado del ejercicio 9 10 y 11 devuelta

--- BD/db/Practica 2 sentencias JOINS.sql
+++ BD/db/Practica 2 sentencias JOINS.sql
@@ -1,7 +1,7 @@
--- Active: 1719338576084@@127.0.0.1@3306@agencia_personal
+-- Active: 1721298484659@@127.0.0.1@3306@agencia_personal
 use agencia_personal;
 
--- Ejercicio 1 🕛
+-- Ejercicio 1 ✅
 -- Mostrar del Contrato 5:  DNI, Apellido y Nombre de la persona contratada y el sueldo acordado en el contrato.
 
 SELECT dni_persona, sueldo, nro_contrato
@@ -10,7 +10,7 @@ INNER JOIN personas p
 ON p.dni = c.dni_persona
 WHERE c.nro_contrato = 5;
 
--- Ejercicio 2 🕛
+-- Ejercicio 2 ✅
 -- ¿Quiénes fueron contratados por la empresa Viejos Amigos o Tráigame Eso? 
 -- Mostrar el DNI, número de contrato, fecha de incorporación, fecha de solicitud en la agencia de los contratados y 
 -- fecha de caducidad (si no tiene fecha de caducidad colocar ‘Sin Fecha’). 
@@ -23,7 +23,7 @@ ON c.cuit = e.cuit_empresa
 WHERE e.razon_social = 'Viejos Amigos' OR e.razon_social = 'Tráigame Eso'
 ORDER BY fecha_incorporacion, e.razon_social;
 
--- Ejercicio 3 🕛
+-- Ejercicio 3 ✅
 -- Listado de las solicitudes consignando razón social, dirección y e_mail de la empresa, 
 -- descripción del cargo solicitado y años de experiencia solicitados, ordenado por fecha de solicitud y 
 -- descripción de cargo.
@@ -33,7 +33,7 @@ FROM solicitudes_empresas s JOIN empresas e JOIN cargos c
 ON s.cuit = e.cuit_empresa AND s.cod_cargo = c.cod
 ORDER BY fecha_solicitud, desc_cargo;
 
--- Ejercicio 4 🕛
+-- Ejercicio 4 ✅
 -- ¿Listar todos los candidatos con título de bachiller o un título de educación no formal. 
 -- Mostrar nombre y apellido, descripción del título y DNI.
 
@@ -42,7 +42,7 @@ FROM personas p JOIN titulos t JOIN personas_titulos pt
 ON p.dni = pt.dni_persona AND pt.cod_titulo = t.cod
 WHERE tipo_titulo = 'Secundario' OR tipo_titulo = 'Educación no formal';
 
--- Ejercicio 5 🕛
+-- Ejercicio 5 ✅
 -- Realizar el punto 4 sin mostrar el campo DNI pero para todos los títulos.
 
 SELECT nombre, apellido, desc_titulo
@@ -50,7 +50,7 @@ FROM personas p INNER JOIN titulos t JOIN personas_titulos pt
 ON p.dni = pt.dni_persona AND pt.cod_titulo = t.cod
 WHERE tipo_titulo = 'Secundario' OR tipo_titulo = 'Educación no formal';
 
--- Ejercicio 6 🕛
+-- Ejercicio 6 ✅
 -- Empleados que no tengan referencias o hayan puesto de referencia a Armando Esteban Quito o Felipe Rojas. 
 -- Mostrarlos de la siguiente forma:
 -- Pérez, Juan tiene como referencia a Felipe Rojas cuando trabajo en Constructora Gaia S.A
@@ -65,14 +65,15 @@ SELECT DISTINCT CONCAT(apellido, ', ', nombre, ' tiene como referencia a ', pers
 FROM tabla_temporal tt JOIN contratos c JOIN empresas e
 ON tt.dni = c.dni_persona AND c.cuit = e.cuit_empresa;
 
--- Ejercicio 7 🕛
+-- Ejercicio 7 ✅
 -- Seleccionar para la empresa Viejos amigos, fechas de solicitudes, descripción del cargo solicitado y 
 -- edad máxima  y mínima . Si no tiene edad mínima y máxima indicar “sin especificar”. 
 -- Encabezado:
 -- Empresa | Fecha de Solicitud | Cargo Solicitado | Edad Mínima | Edad Máxima
 
-SELECT 
-    razon_social as 'Empresa', fecha_solicitud as 'Fecha de Solicitud', 
+
+SELECT
+    razon_social as 'Empresa', fecha_solicitud as 'Fecha de Solicitud',
     desc_cargo as 'Cargo Solicitado',
     CASE  
         WHEN edad_minima IS NULL THEN 'Sin especificar'
@@ -85,11 +86,11 @@ SELECT
 FROM    
     solicitudes_empresas s INNER JOIN empresas e INNER JOIN cargos c
 ON 
-    s.cuit = e.cuit_empresa AND s.cod_cargo = c.cod
+    s.cuit = e.cuit AND s.cod_cargo = c.cod_cargo
 WHERE 
     razon_social = 'Viejos Amigos';
 
--- Ejercicio 8 🕛
+-- Ejercicio 8 ✅
 -- Mostrar los antecedentes de cada postulante:
 -- Postulante(nombre y apellido) | Cargo(descripcion del cargo)
 
@@ -98,9 +99,9 @@ SELECT
 FROM 
     antecedentes a INNER JOIN personas p INNER JOIN cargos c
 ON 
-    a.dni_persona = p.dni AND a.cod_cargo = c.cod;
+    a.dni = p.dni AND a.cod_cargo = c.cod_cargo;
 
--- Ejercicio 9 🕛
+-- Ejercicio 9 ✅
 -- Mostrar todas las evaluaciones realizadas para cada solicitud ordenar en forma ascendente por empresa y 
 -- descendente por cargo:
 -- Empresa | Cargo | Desc Evaluacion | Resultado
@@ -110,6 +111,43 @@ SELECT
 FROM
     entrevistas e INNER JOIN entrevistas_evaluaciones ee INNER JOIN evaluaciones ev INNER JOIN empresas em INNER JOIN cargos c
 ON 
-    e.nro_entrevista = ee.nro_entrevista AND ee.cod_evaluacion = ev.cod AND e.cuit = em.cuit_empresa AND e.cod_cargo = c.cod
+    e.nro_entrevista = ee.nro_entrevista AND ee.cod_evaluacion = ev.cod_evaluacion AND e.cuit = em.cuit AND e.cod_cargo = c.cod_cargo
 ORDER BY
     razon_social ASC, desc_cargo DESC;
+
+
+-- Ejercicio 10 ✅
+-- Listar las empresas solicitantes mostrando la razón social y fecha de cada solicitud, 
+-- y descripción del cargo solicitado. Si hay empresas que no hayan solicitado que salga la leyenda: 
+-- "Sin Solicitudes" en la fecha y en la descripción del cargo.
+
+SELECT
+    e.cuit as 'Cuit', razon_social as 'Empresa',
+    CASE 
+        WHEN fecha_solicitud IS NULL THEN 'Sin Solicitudes'
+        ELSE fecha_solicitud
+        END as 'Fecha de Solicitud',
+    CASE
+        WHEN desc_cargo IS NULL THEN 'Sin Solicitudes'
+        ELSE desc_cargo
+        END as 'Cargo Solicitado'
+FROM 
+    empresas e 
+    LEFT JOIN solicitudes_empresas s ON s.cuit = e.cuit
+    LEFT JOIN cargos c ON c.cod_cargo = s.cod_cargo
+
+-- Ejercicio 11 ✅
+-- Mostrar para todas las solicitudes la razón social de la empresa solicitante,
+-- el cargo y si se hubiese realizado un contrato los datos de la(s) persona(s).
+
+-- Comentarios 💬
+-- Me trae una instancia de mas ya que tiene dos contratos la misma persona, uno que finalizo y otro que no
+
+SELECT 
+    e.cuit, razon_social, desc_cargo, ifnull(nombre, 'Sin contrato'), ifnull(apellido, 'Sin contrato'), ifnull(dni, 'Sin contrato')
+FROM 
+    solicitudes_empresas s
+    LEFT JOIN empresas e ON s.cuit = e.cuit
+    LEFT JOIN contratos co ON s.fecha_solicitud = co.fecha_solicitud AND s.cod_cargo = co.cod_cargo AND s.cuit = co.cuit
+    LEFT JOIN personas p ON p.dni = co.dni_persona
+    LEFT JOIN cargos c ON c.cod_cargo = s.cod_cargo;
