bf204401553297d8ac1768f73f34aaa776af7120
Elias Palamidessi <101844004+Eliasusu@users.noreply.github.com>
2024-08-27 20:39:06 +0000

    Update Practica 5 Variables y tablas temporales.sql

--- BD/practicas/Practica 5 Variables y tablas temporales.sql
+++ BD/practicas/Practica 5 Variables y tablas temporales.sql
@@ -28,3 +28,20 @@ select distinct per.dni, per.nombre, per.apellido , con.cuit
 from contratos con
 inner join personas per on con.dni_persona = per.dni
 where con.cuit = @cuitEmpresa;
+
+-- Ejercicio 2 
+-- Encontrar a aquellos empleados que ganan menos que el máximo sueldo de los empleados de Viejos Amigos. 
+
+-- Ejercicio 5 
+-- Determinar las empresas que pagaron en promedio la mayor de las comisiones.
+
+set @promedioComisiones = (
+select avg(c.importe_comision) 
+from comisiones c
+);
+
+select em.razon_social, avg(co.importe_comision) EstaPagaBien
+from contratos c
+inner join comisiones co on c.nro_contrato = co.nro_contrato
+inner join empresas em on c.cuit = em.cuit
+group by 1;
